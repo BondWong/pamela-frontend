cb8f9f59ab97ebd6f98ea8345a529eb354fe99b1
JavaEEBoy <WongZeonbong@gmail.com>
2015-06-03 14:50:13 +0800

    Initial Commit

---
+++ Pamela/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pamela
+//
+//  Created by Bond Wong on 6/3/15.
+//  Copyright (c) 2015 Bond Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
